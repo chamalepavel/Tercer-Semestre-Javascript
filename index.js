0fde6b1240f585b801f6ec74f01fcfc545ad2ea7
PavelintheMatrix <166340133+chamalepavel@users.noreply.github.com>
2025-02-16 17:43:46 -0800

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function changeGreeting(text) {
-    document.getElementById('greeting').textContent = text;
-}
+document.addEventListener("DOMContentLoaded", function() {
+    let titulo = document.getElementById("titulo");
+
+    document.getElementById("btnDom").addEventListener("click", function() {
+        titulo.textContent = "Hello Dom!";
+    });
+
+    document.getElementById("btnWorld").addEventListener("click", function() {
+        titulo.textContent = "Hello Worl!";
+    });
+
+    document.getElementById("btnBoth").addEventListener("click", function() {
+        titulo.textContent = "Hello Worl! Hello DOM!";
+    });
+});
